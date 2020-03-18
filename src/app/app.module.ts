8a1ac31eb361488a0bccb55471ab606823240e38
Osvaldo González Venegas <ojgonzalez@falabella.cl>
2020-03-18 00:58:19 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TechnologyCardComponent } from './components/technology-card/technology-card.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SearchComponent } from './pages/search/search.component';
+import { TechnologyComponent } from './pages/technology/technology.component';
+import { TechnologiesComponent } from './pages/technologies/technologies.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TechnologyCardComponent,
+    AboutComponent,
+    SearchComponent,
+    TechnologyComponent,
+    TechnologiesComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
